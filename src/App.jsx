8de5ef584ca5061b84a7b6209cce76358edf8bef
Kelzem1 <rodolfomews@gmail.com>
2023-04-10 00:20:43 +0200

    Tailwindcss and more

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,22 @@ import { TaskCreator } from './components/TaskCreator'
 
 
 function App() {
+
+  const [taskItems, newTaskItems] = useState([
+    { name: 'mi primer tarea', done: false},
+    { name: 'mi segunda tarea', done: false},
+    { name: 'mi tercera tarea', done: false},
+  ])
     return (
-      <div className='App'>
-        <TaskCreator/>
+      <div className="App bg-slate-900 text-white object-center">
+        <TaskCreator />
+        {
+          taskItems.map(task => (
+            <div>
+            {task.name}
+            </div>
+          ))
+        }
       </div>
 
   )
