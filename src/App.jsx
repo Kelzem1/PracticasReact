3a697115b73e3dc95bc442341701222fd8a0ac5b
Kelzem1 <kelzemrod@gmail.com>
2023-04-11 01:25:01 +0200

    No react strict mode, localstorage, alert about same taks

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { TaskCreator } from './components/TaskCreator'
 
 
 
 function App() {
 
-  const [taskItems, newTaskItems] = useState([
-    { name: 'mi primer tarea', done: false},
-    { name: 'mi segunda tarea', done: false},
-    { name: 'mi tercera tarea', done: false},
-  ])
+  const [taskItems, newTaskItems] = useState([])
 
   function createTask(taskName) {
-    if(!taskItems.find(task => task.name === taskName))
+    if(!taskItems.find(task => task.name === taskName)){
       newTaskItems([...taskItems, {name: taskName, done: false}])
-  } 
+    }else{
+      alert("Ya existe esta tarea")
+    }
+  }
+
+  useEffect(() => {
+    let data = localStorage.getItem('tasks')
+    if (data) {
+      newTaskItems(JSON.parse(data))
+    }
+  }, [])
+
+//Guardara las nueva tareas
+  useEffect( () =>{
+    localStorage.setItem('tasks' , JSON.stringify(taskItems))
+  }, [ taskItems ])
+
+
     return (
       <div className="App bg-cyan-950 text-cyan-600 w-4/4 h-screen content-center m-auto p-32">
         <TaskCreator createNewTask={createTask}/>
