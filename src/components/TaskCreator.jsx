cae13e0b3b3479b2a22c821f4c60c175ef7b7489
Kelzem1 <kelzemrod@gmail.com>
2023-04-19 22:43:34 +0200

    styles changes

--- src/components/TaskCreator.jsx
+++ src/components/TaskCreator.jsx
@@ -17,12 +17,12 @@ export const TaskCreator = ( {createNewTask} ) =>{
     <div className="formu m-auto content-center">
         <form onSubmit={handleSubmit}>
         <input type='text'
-        className="input text-black bold placeholder:italic placeholder:text-slate-400 m-auto content-center block box-border w-1/2 bg-white border border-slate-100 rounded py-2 pl-2 pr-3 shadow-inner focus:outline-none focus:border-gray-300 focus:ring-gray-200 focus:ring-1 sm:text-sm" name="search"
+        className="input text-black bold placeholder:italic placeholder:text-slate-400 m-auto content-center block box-border w-1/2 bg-white border border-slate-100 rounded py-2 pl-2 pr-3 shadow shadow-teal-700 focus:outline-none focus:border-gray-300 focus:ring-gray-200 focus:ring-1 sm:text-sm" name="search"
         placeholder='Enter a new task'
         value={newTaksName}
         onChange={(e) => setNewTaksName(e.target.value)}>
         </input>
-        <button className="flex m-auto mt-3 content-center bg-slate-100 hover:bg-teal-600 text-black font-bold py-2 px-4 border-b-4 border-teal-700 hover:border-slate-300 rounded transition duration-75 ease-in-out hover:-translate-y-1 hover:scale-100">Add task</button>
+        <button className="flex m-auto mt-3 content-center bg-teal-50 shadow-lg hover:bg-teal-600 text-black font-bold py-2 px-4 border-b-4 border-teal-700 hover:border-slate-300 rounded transition duration-75 ease-in-out hover:-translate-y-1 hover:scale-100">Add task</button>
         </form>
     </div>
     )
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 export const Titu =() =>{
     return(
-        <div className=" block content-center my-20 mt-0">
-            <h1 className="text-5xl text-black text-center font-extrabold">2DU</h1>
+        <div className="block content-center my-20 mt-1">
+            <h1 className="text-5xl text-teal-900 text-center font-extrabold tracking-wider">2-DU</h1>
         </div>
     )
 }
